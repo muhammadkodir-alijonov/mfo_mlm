87aa18d0cea99207a84b87337d9f5caeb39def62
muxammadqodir.a <muxammadqodir.alijonov@fido.uz>
2025-01-15 10:54:06 +0500

    v05

--- test_mlm/output/Ln_Front_mlm.sql
+++ test_mlm/output/Ln_Front_mlm.sql
@@ -2,100 +2,15 @@
 begin
 mlm.save_template(
     i_module_code => 'LN',
-    i_message_name => 'LN',
+    i_message_name => 'THE_SPECIFIED_CLIENT_1_IS_INACTIVE_OR_NOT_A_CLIENT',
     i_message_type => 'T',
     i_error_code => 1,
-    i_param_count => 0,
+    i_param_count => 1,
     i_state => 'A',
-    i_message_mask => mlm_label_t('LN', 'ЛН', 'LN', 'LN'),
+    i_message_mask => mlm_label_t('Указанный клиент $1 находится в состоянии НЕ АКТИВКЕН или НЕ ЯВЛЯЕТСЯ клиентом вашего филиала. см. Клиенты и счета!', 'Бэлгиланган $1 мижоз НОФАОЛ ёки филиалингиз мижози ЭМАС Мижозлар ва Ҳисобларга қаранг!', 'Belgilangan $1 mijoz NOFAOL yoki filialingiz mijozi EMAS Mijozlar va Hisoblarga qarang!', 'The specified client $1 is INACTIVE or NOT a client of your branch see Clients and Accounts!'),
     i_user_action => mlm_text_t(),
     i_admin_action => mlm_text_t(),
-    i_description => 'Ln_Front.pck:1'
-);
-commit;
-end;
-/
-
-begin
-mlm.save_template(
-    i_module_code => 'LN',
-    i_message_name => 'LN',
-    i_message_type => 'T',
-    i_error_code => 1,
-    i_param_count => 0,
-    i_state => 'A',
-    i_message_mask => mlm_label_t('LN', 'ЛН', 'LN', 'LN'),
-    i_user_action => mlm_text_t(),
-    i_admin_action => mlm_text_t(),
-    i_description => 'Ln_Front.pck:32'
-);
-commit;
-end;
-/
-
-begin
-mlm.save_template(
-    i_module_code => 'LN',
-    i_message_name => 'LN',
-    i_message_type => 'T',
-    i_error_code => 1,
-    i_param_count => 0,
-    i_state => 'A',
-    i_message_mask => mlm_label_t('LN', 'ЛН', 'LN', 'LN'),
-    i_user_action => mlm_text_t(),
-    i_admin_action => mlm_text_t(),
-    i_description => 'Ln_Front.pck:48'
-);
-commit;
-end;
-/
-
-begin
-mlm.save_template(
-    i_module_code => 'LN',
-    i_message_name => 'LN',
-    i_message_type => 'T',
-    i_error_code => 1,
-    i_param_count => 0,
-    i_state => 'A',
-    i_message_mask => mlm_label_t('LN', 'ЛН', 'LN', 'LN'),
-    i_user_action => mlm_text_t(),
-    i_admin_action => mlm_text_t(),
-    i_description => 'Ln_Front.pck:63'
-);
-commit;
-end;
-/
-
-begin
-mlm.save_template(
-    i_module_code => 'LN',
-    i_message_name => 'LINENBLANK',
-    i_message_type => 'T',
-    i_error_code => 1,
-    i_param_count => 0,
-    i_state => 'A',
-    i_message_mask => mlm_label_t('LNBLANK', 'ЛинэнБланк', 'LinenBlank', 'LinenBlank'),
-    i_user_action => mlm_text_t(),
-    i_admin_action => mlm_text_t(),
-    i_description => 'Ln_Front.pck:81'
-);
-commit;
-end;
-/
-
-begin
-mlm.save_template(
-    i_module_code => 'LN',
-    i_message_name => 'LN',
-    i_message_type => 'T',
-    i_error_code => 1,
-    i_param_count => 0,
-    i_state => 'A',
-    i_message_mask => mlm_label_t('LN', 'ЛН', 'LN', 'LN'),
-    i_user_action => mlm_text_t(),
-    i_admin_action => mlm_text_t(),
-    i_description => 'Ln_Front.pck:97'
+    i_description => 'Ln_Front.pck:4'
 );
 commit;
 end;
